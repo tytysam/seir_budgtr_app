e63d2dc70f4854588b92e4b52c9ddaaecd3635be
Tyler Samuelson <tylercsamuelson@gmail.com>
2020-12-01 17:02:09 -0700

    MVC file structure built

---
+++ budgtr/server.js
@@ -0,0 +1,47 @@
+// =======================================
+//              DEPENDENCIES
+// =======================================
+const { urlencoded } = require("express");
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+// =======================================
+//                DATABASE
+// =======================================
+const budgets = require("./models/budget.js");
+
+// =======================================
+//               MIDDLEWARE
+// =======================================
+app.set("view engine", "jsx");
+app.engine("jsx", require("express-react-views").createEngine());
+app.use(express.urlencoded({ extended: false }));
+
+// =======================================
+//                ROUTES
+// =======================================
+
+// Route Handler | Index ==> GET ==> Presentational Route
+// ==> /budgets
+app.get("/budgets/", (req, res) => {
+  res.render("Index", {
+    budgets: budgets,
+  });
+});
+
+// Route Handler | New ==> GET ==> Presentational Route
+// ==> /budgets/new
+
+// Route Handler | Create ==> POST ==> Functional Route
+// ==> /budgets
+
+// Route Handler | Show ==> GET ==> Presentational Route
+// ==> /budgets/:id
+
+// =======================================
+//              LISTENER
+// =======================================
+app.listen(PORT, () => {
+  console.log(`Budgtr App listening on port: ${PORT}`);
+});
---
+++ budgtr/views/Index.jsx
@@ -0,0 +1,9 @@
+const React = require("react");
+
+class Index extends React.Component {
+  render() {
+    return <h1>Hello World</h1>;
+  }
+}
+
+module.exports = Index;
